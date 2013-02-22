91f8e055e92ea5a60a09ff65ad5a292a112e5bb7
Dmitry Kamenka <mail@traiden.org>
2013-02-22 16:12:41 +0300

    Show errors on upload

--- app/controllers/ckeditor/application_controller.rb
+++ app/controllers/ckeditor/application_controller.rb
@@ -1,12 +1,12 @@
 class Ckeditor::ApplicationController < ::ApplicationController
   respond_to :html, :json
-  
+
   before_filter :find_asset, :only => [:destroy]
   before_filter :ckeditor_authorize!
   before_filter :authorize_resource
 
   protected
-    
+
     def respond_with_asset(asset)
       file = params[:CKEditor].blank? ? params[:qqfile] : params[:upload]
       asset.data = Ckeditor::Http.normalize_param(file, request)
@@ -17,10 +17,14 @@ class Ckeditor::ApplicationController < ::ApplicationController
         body = params[:CKEditor].blank? ? asset.to_json(:only=>[:id, :type]) : %Q"<script type='text/javascript'>
           window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '#{Ckeditor::Utils.escape_single_quotes(asset.url_content)}');
         </script>"
-        
-        render :text => body
       else
-        render :nothing => true
+        errors = asset.errors.full_messages.reject{ |m| m =~ /Paperclip/ }.join("\n")
+        body = params[:CKEditor].blank? ? '' : %Q"<script type='text/javascript'>
+          window.parent.CKEDITOR.tools.callFunction(#{params[:CKEditorFuncNum]}, '',
+            '#{Ckeditor::Utils.escape_single_quotes(errors)}');
+        </script>"
       end
+
+      render :text => body
     end
 end
